b36636974e0b3b1fea4ef82cb77a3287656ba477
Mattias Tüür <mattiastuur@gmail.com>
2024-02-14 10:27:01 +0200

    Informative text in homepage.

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
   <HeaderFile />
   <h1 class="homePageHeader">Welcome to the homepage</h1>
+  <p class="text">This is the test project for making a better interface for users,</p>
+  <p class="text">who may choose different sectors in which they are currently involved in.</p>
 </template>
 
 <script>
@@ -26,5 +28,10 @@ export default {
 .homePageHeader{
   font-size: 40px;
   color: midnightblue;
+  padding-bottom: 100px;
+}
+.text{
+  font-size: 30px;
+  color: darkgreen;
 }
 </style>
